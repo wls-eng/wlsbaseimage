72fc6b9c5b1ad3539276919fe0aeba45f36eaf0e
work@jacobthomas.net <work@jacobthomas.net>
2020-12-02 17:55:41 -0800

    debug

--- installpatch.sh
+++ installpatch.sh
@@ -455,7 +455,7 @@ echo "Patching OPatch ...."$PATCH_DIR/$patcharchive
 
 runuser -l oracle -c "$JAVA_HOME/bin/java -jar  $PATCH_DIR/$patcharchive/opatch_generic.jar -silent oracle_home=/u01/app/wls/install/oracle/middleware/oracle_home"
 
-#downloadpatch $wlspsu_patch  "wlspsu.zip"
+downloadpatch $wlspsu_patch  "wlspsu.zip"
 
 unzippatch $PATCH_DIR/wlspsu.zip
 
